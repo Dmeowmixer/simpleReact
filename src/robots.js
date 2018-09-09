728b5b5afd3d11ea6c7c7e5979dbdd8e59fa3fc2
Dmeowmixer <ransonikuo@gmail.com>
2018-09-08 23:00:17 -1000

    simple react demo app

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Son Goku',
+    username: 'Goku',
+    email: 'songoku@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Vegeta',
+    username: 'Vegeta',
+    email: 'princeofsaiyans@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Son Gohan',
+    username: 'Gohan',
+    email: 'thegreatsaiyaman@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Trunks',
+    username: 'Trunks',
+    email: 'Trunks@capsule.co'
+  },
+  {
+    id: 5,
+    name: 'Bulma',
+    username: 'Bulma',
+    email: 'Bulma@capsule.co'
+  },
+  {
+    id: 6,
+    name: 'Chichi',
+    username: 'Chichi',
+    email: 'chichi@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Piccolo',
+    username: 'Piccolo',
+    email: 'Piccolo@bugmenot.com'
+  },
+  {
+    id: 8,
+    name: 'Yamcha',
+    username: 'Yamcha',
+    email: 'baseballer@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Yajirobi',
+    username: 'Yajirobi',
+    email: 'imhungry@gmail.com'
+  }
+]
